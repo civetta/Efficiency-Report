2d26201b2569b02bb3e8be18de04c61f8cb2be50
Civetta <civettaxx@gmail.com>
2018-05-03 09:51:20 -0400

    Cleaned Up Archive.py

--- Archive.py
+++ Archive.py
@@ -1,85 +1,65 @@
 from openpyxl import Workbook
 import os
 import gspread
-import time
 import oauth2client   
 
 #Completes all of the authorization for google sheets
-def authorize():
+def authorize(name_of_gsheet):
     from oauth2client.service_account import ServiceAccountCredentials
     scope = "https://spreadsheets.google.com/feeds"
     credentials = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\kheyden\\Documents\\Program\\archivekey.json', scope)
     gs = gspread.authorize(credentials)
     gsheet = gs.open("Test")
-    wsheet = gsheet.worksheet("apr23")
+    wsheet = gsheet.worksheet(name_of_gsheet)
     return wsheet
-
-
-#The below three functions are utility functions that will be used repeatedily 
-#########################################################################################
-#Creates excel workbook
-def create_workbooks():
-    wb = Workbook()
-    return wb
 
 #Creates excel worksheet
 def creat_worksheets(wb):
     ws=wb.create_sheet('Raw Pulls')
     return ws
-
-#Saves the workbook    
-"""def workbook_save(wb,ws):
-        test_save_name = os.path.join('C:\\Users\\kheyden\\Documents\\Program\\2017\\WeeklySummary\\', 'Leadbook' + '.xlsx')
-        wb.save(test_save_name)"""
-#########################################################################################
-
         
 #Takes what is inside of the google spreadsheet and pastes it inside of the excel sheet   
-def copy_in_excel(wsheet,wb,ws,ranger,n):
+def copy_into_excel(wsheet,ws,ranger,n):
     col_count = wsheet.col_count
     col = ranger[0]+1
     while col <= ranger[1]:
         excel_col=ws.max_column
-        
         list_of_values = wsheet.col_values(col)
-        for i in list_of_values:
-            try:
-                i=int(i)
-            except:
-                i=str(i)
+        list_of_values=convert_type(list_of_values)
         row_count = len(list_of_values)
         row = 1
         while row < row_count:     
             ws.cell(row = row, column = excel_col+n,value = list_of_values[row-1])
-            
             row = row+1
         col = col+1
         n=1
-    #workbook_save(wb,ws)
+        
+#Converts all of the types from gsheet into correct types (string vs integer) so they are formatted in excel correctly.
+def convert_type(list_of_values):
+    for i in list_of_values:
+            try:
+                i=int(i)
+            except:
+                i=str(i)
+    return list_of_values
 
-
+#Find the first teacher and last teacher in the team, using team Leads as the splitters
 def find_team_range(wsheet,start,end):
     names=wsheet.row_values(1)
     start=names.index(start)
     end=names.index(end)
     return [start,end]
-    
-#Removes all of the data from the google spreadsheet, leaving only the top row with labels, and 3 empty rows
-def wsheet_clean_up(wsheet):
-    wsheet.resize(rows = 1,cols = wsheet.col_count)
-    wsheet.add_rows(3)
 
 #Calls all of the other functions above
-def call_function(start,end):
-    wsheet = authorize()
-    wb=create_workbooks()
+def archive_to_excel(start,end,name_of_gsheet):
+    wsheet = authorize(name_of_gsheet)
+    wb = Workbook()
     ws=creat_worksheets(wb)
-    #workbook_save(wb,ws)
     ranger=find_team_range(wsheet,start,end)
-    copy_in_excel(wsheet,wb,ws,[0,2],0)
-    copy_in_excel(wsheet,wb,ws,ranger,1)
-    #workbook_save(wb,ws)
-    #wsheet_clean_up(wsheet)
+    #Copies the date and tabby into excel
+    copy_into_excel(wsheet,ws,[0,2],0)
+    #Copies teachers data into excel
+    copy_into_excel(wsheet,ws,ranger,1)
     return wb
 
 
--- call_module.py
+++ call_module.py
@@ -9,7 +9,7 @@ from efficency_calculator import create_block_table
 from break_down import create_summary
 from weekly_summary import find_row
 from time_difference import make_sheet
-from Archive import call_function
+from Archive import archive_to_excel
 from formatter import formatter
 from teacher_books import create_books
 
@@ -31,13 +31,13 @@ def clean_up(wb):
 
 def save_files(wb,Lead_Name):
     date=get_date(wb)
-    folder_location = os.path.join('C:\Users\kheyden\OneDrive - Imagine Learning\Reports\Efficiency Reports',Lead_Name)
+    folder_location = os.path.join('C:\Users\kheyden\OneDrive - Imagine Learning\Reports\Efficiency Reports',"TEST")
     create_dir(folder_location)
     lead_book_location=os.path.join(folder_location,"TEAM E-REPORTS")
     create_dir(lead_book_location)
-    final_save_name = os.path.join(lead_book_location,Lead_Name+"_"+date+"-TEAMWIDE.xlsx")
+    final_save_name = os.path.join(lead_book_location,Lead_Name+"_"+date+"-LEADBOOK.xlsx")
     wb.save(final_save_name)
-    create_books(max_col,wb,folder_location,date)
+    #create_books(max_col,wb,folder_location,date)
     
 
 def get_date(wb):
@@ -48,21 +48,22 @@ def get_date(wb):
     return date
 
 
-Lead_Names=["Jeremy Shock",'']
+Lead_Names=["Salome Saenz","Kristin Donnelly"]
 #"Jeremy Shock","Rachel Adams","Jairo  Rios","Salome Saenz","Kristin Donnelly","Caren Glowa",''
 for x in range(len(Lead_Names)-1):
     print "------------------------------------------------------------"
     print Lead_Names[x]
-    start=Lead_Names[x]
-    end=Lead_Names[x+1]
+    start_of_team=Lead_Names[x]
+    end_of_team=Lead_Names[x+1]
     current_lead=Lead_Names[x]
     if current_lead=="Salome Saenz":
         current_lead="Jill Szafranski"
-    wb = call_function(start,end)
+    name_of_gsheet="apr23"
+    wb = archive_to_excel(start_of_team,end_of_team,name_of_gsheet)
     wb=make_sheet(wb)
     wb=find_days(wb)
     wb=define_blocks(wb)
-    max_col=create_block_table(wb,start)
+    max_col=create_block_table(wb,start_of_team)
     create_summary(wb,max_col)
     find_row(wb)
     std=wb.get_sheet_by_name('Sheet')
