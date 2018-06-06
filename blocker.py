38212829315d56e2e035e7c3655b051ff4a6f4e5
Civetta <civettaxx@gmail.com>
2018-06-06 12:48:09 -0400

    removed repetitive code and shortened name of variables

--- blocker.py
+++ blocker.py
@@ -3,30 +3,30 @@ from openpyxl.styles import PatternFill
 
 
 def define_blocks(wb):
-   """Goes through each ws and then goes through each column looking for the 
-   start and end of each block. It then calls the bolder function conditionally
-   format the cells"""
+    """Goes through each ws and then goes through each column looking for the 
+    start and end of each block. It then calls the bolder function
+    conditionally format the cells"""
     week = wb.get_sheet_names()
     week = week[:-3]
     for day in week:
         ws = wb.get_sheet_by_name(day)
         max_row, max_col = ws.max_row, ws.max_column
         col = 8
-        starting_row_to_look = 2
+        start_row_to_look = 2
         while col <= max_col:
-            start = find_position_of_blocks(ws, col, max_row, starting_row_to_look, 'start')
-            if start >= starting_row_to_look and start != max_row:
-                end = find_position_of_blocks(ws, col, max_row, start, 'end')
-                starting_row_to_look = end
+            start = find_blocks(ws, col, max_row, start_row_to_look, 'start')
+            if start >= start_row_to_look and start != max_row:
+                end = find_blocks(ws, col, max_row, start, 'end')
+                start_row_to_look = end
                 bolder(ws, start, end, col, max_col)                  
             else:
                 col = col+1
-                starting_row_to_look = 2
+                start_row_to_look = 2
     return wb
 
 
-def find_position_of_blocks(ws, col, max_row, starting_row, position):
-    """Looks for either two 0's in a row for the end of a block, or 
+def find_blocks(ws, col, max_row, starting_row, position):
+    """Looks for either two 0's in a row for the end of a block,or 
     two sequential non zeros for the start of the block"""
     for a in range(starting_row, max_row):
         val1 = ws.cell(row=a, column=col).value
@@ -44,31 +44,26 @@ def find_position_of_blocks(ws, col, max_row, starting_row, position):
                     return a
     return max_row
 
+
 def bolder(ws, start_index, end_index, column, max_col):
     """This function goes through and bolds and conditionally
     formatts each of the blocks. The bolding will be used later to identify
     a block and for calculating"""            
     tab_list = []
     for r in range(start_index, end_index):
-        Tabby_Cell = ws.cell(row=r,  column=7).value
-        Tabby_Cell = int(Tabby_Cell)
+        Tabby_Cell = int(ws.cell(row=r,  column=7).value)
         tab_list.append(Tabby_Cell)
         current_cell = ws.cell(row=r,  column=column)
         current_value = current_cell.value
         plus_1_check = current_value == Tabby_Cell+1 
         minus_1_check = current_value == Tabby_Cell-1
-        if current_value == 0:
-            current_cell.fill = PatternFill("solid", fgColor='ffffff')
-            current_cell.font = Font(bold=True)
-        elif current_value == Tabby_Cell or plus_1_check or minus_1_check:
+        current_cell.font = Font(bold=True)
+        if current_value == Tabby_Cell or plus_1_check or minus_1_check:
             current_cell.fill = PatternFill("solid", fgColor='dff7c0')
-            current_cell.font = Font(bold=True)
         elif current_value < Tabby_Cell and current_value > 0:
             current_cell.fill = PatternFill("solid", fgColor='f2b8ea')
-            current_cell.font = Font(bold=True)
         elif current_value >= Tabby_Cell+2:
             current_cell.fill = PatternFill("solid", fgColor='c0f7f4')
-            current_cell.font = Font(bold=True)
         else:
                 continue
     #organize_data(ws, start_index, end_index, column, block, tab_list, max_col)
