e90b04d461742527e8eaba73e54c454af6744d61
Civetta <civettaxx@gmail.com>
2018-06-14 14:05:08 -0400

    more comments

--- create_tables.py
+++ create_tables.py
@@ -4,6 +4,8 @@ from openpyxl.utils import get_column_letter
 
 
 def call_create_tables(wb):
+    """Goes through each day of the week worksheet and calls the functions
+    below"""
     week = wb.get_sheet_names()
     week = week[:-2]
     for day in week:
@@ -14,18 +16,28 @@ def call_create_tables(wb):
 
 
 def create_daily_tables(ws, max_col):
-        for column in range(8, max_col+1):
-            table_start_cell = ((column-7)*8)-6
-            teacher_name = ws.cell(row=1, column=column).value
-            title_list = [teacher_name, "Students", "Tabby", "Efficiency Score"]
-            for i in range(4):
-                current_cell = ws.cell(row=table_start_cell, column=i+1)
-                current_cell.value = title_list[i]
-                current_cell.font = Font(bold=True)
-                current_cell.fill = PatternFill("solid", fgColor='F2F2F2')
-                
+    """So we go through the teachers in the ws, they start 8 columns in, and
+    use that number to calculate where to put the new table. So we do teacher
+    location column -7, which should give us a 1,2,3 count, and then times 8,
+    so each teacher is located 8 rows apart. Finally we subtract it by 6,
+    so that the tables start higher up in the worksheet versus at row 8.
+    So the first teacher is located at row 2, the second teacher is located at
+    row 10, and so on"""
+    for column in range(8, max_col+1):
+        table_start_cell = ((column-7)*8)-6
+        teacher_name = ws.cell(row=1, column=column).value
+        title_list = [teacher_name, "Students", "Tabby", "Efficiency Score"]
+        for i in range(4):
+            current_cell = ws.cell(row=table_start_cell, column=i+1)
+            current_cell.value = title_list[i]
+            current_cell.font = Font(bold=True)
+            current_cell.fill = PatternFill("solid", fgColor='F2F2F2')
+            
 
 def format_sheet(ws):
+    """Goes through and grays out the spacer column. Makes the time stamp 
+    columns extra wide so they are easy to read, and make all other columns
+    at least 20 px in width"""
     for i in range(1, ws.max_row):
         current_cell = ws.cell(row=i, column=5)
         current_cell.fill = PatternFill("solid", fgColor='F2F2F2')
