664aeae9bbf08ee915cf0433f9a20f3343b0ea62
Kelly Richardson <civettaxx@gmail.com>
2019-01-04 08:41:15 -0500

    cleaned up old version to not include new callmodule re-write so changes are easier to see.

--- callmodule.py
+++ callmodule.py
@@ -1,6 +1,5 @@
 from openpyxl import load_workbook
 from periscope_source import create_input
-from time_difference import make_time_difference_sheet
 from split_days import split_sheet_by_days
 from daily_ws.create_tables import call_create_tables
 from daily_ws.mark_blocks import define_blocks
@@ -30,15 +29,13 @@ def save_leadbook(wb,save_date,debug, lead_name):
         wb.save('Output/Teacher Books/LEADBOOK_'+lead_name+"_"+save_date+'.xlsx')
         
         
+
 """INPUTS HERE"""
-"""Jeremy Shock, Rachel Adams,Jill Szafranski,Kristin Donnelly, Melissa Cox,Caren Glowa, All"""
+"""Jeremy Shock, Rachel Adams,Melissa Cox, Jill Szafranski,Kristin Donnelly,Caren Glowa, All"""
 #Uses Periscope Source and Tabby source to format and make the raw changes sheet in lead book.
 lead_name = "All"
-periscope = 'all_108.csv'
-tabby = 'tabby_108.csv'
-create_input(periscope,tabby,lead_name)
-
-
+periscope = 'e-data_source/e-data_Fall/123_all.csv'
+tabby = "e-data_source/e-data_Fall/123_tabby.csv"
 #Skip days are used to skip days with bad data, or to only return certain days from a dataset.
 skip_days = []
 #Used to Conditionally Format the Daily Summary tables
@@ -49,85 +46,9 @@ scores = {"Good Day Score": float(.90), "Upper Bound": float(1.25),
 save_date = "12-17-18"
 #Used to indicate a end of day for split day function.
 end_day_indicator = '12:54 AM'
-debug = False
-
-##Do df[[teachername1,teachername2,teachername3]]
-##Then set teachername1 as leadname
-team_org = [['Jeremy Shock','*SSMax','Jeremy Shock', 'Jennifer Gilmore', 'Kay Plinta-Howard', 'Crystal Boris', 'Melissa Mitchell', 'Cassie Ulisse', 'Laura Gardiner', 'Michelle Amigh', 'Kimberly Stanek'],
-    ['Rachel Adams','*SSMax','Rachel Adams', 'Cristen Phillipsen', 'Heather Chilleo', 'Hester Southerland', 'Jamie Weston', 'Michele  Irwin', 'Juventino Mireles'],
-    ['Melissa Cox','*SSMax','Melissa Cox', 'Clifton Dukes', 'Kelly Richardson', 'Veronica Alvarez', 'Nancy Polhemus', 'Kimberly Abrams', 'Stacy Good'],
-    ['Jill Szafranski','*SSMax','Salome Saenz', 'Alisa Lynch', 'Gabriela Torres', 'Wendy Bowser', 'Nicole Marsula', 'Donita Farmer', 'Andrea Burkholder', 'Laura Craig', 'Bill Hubert', 'Erin Hrncir'],
-    ['Kristin Donnelly','*SSMax','Kristin Donnelly', 'Angel Miller', 'Marcella Parks', 'Sara  Watkins', 'Shannon Stout', 'Lisa Duran', 'Erica Basilone', 'Carol Kish', 'Jennifer Talaski', 'Nicole Knisely'],
-    ['Caren Glowa','*SSMax','Caren Glowa', 'Johana Miller', 'Audrey Rogers', 'Cheri Shively', 'Amy Stayduhar', 'Dominique Huffman', 'Meaghan Wright', 'Kathryn Montano', 'Lynae Shepp', 'Anna Bell', 'Jessica Connole'],
-    ['All','*SSMax','Jeremy Shock', 'Jennifer Gilmore', 'Kay Plinta-Howard', 'Crystal Boris', 'Melissa Mitchell', 'Cassie Ulisse', 'Laura Gardiner', 'Michelle Amigh', 'Kimberly Stanek',
-    'Rachel Adams', 'Cristen Phillipsen', 'Heather Chilleo', 'Hester Southerland', 'Jamie Weston', 'Michele  Irwin', 'Juventino Mireles',
-    'Melissa Cox', 'Clifton Dukes', 'Kelly Richardson', 'Veronica Alvarez', 'Nancy Polhemus', "Kimberly Abrams", 'Stacy Good',
-     'Salome Saenz', 'Alisa Lynch', 'Gabriela Torres', 'Wendy Bowser', 'Nicole Marsula', 'Donita Farmer', 'Andrea Burkholder', 'Laura Craig', 'Bill Hubert', 'Erin Hrncir',
-    'Kristin Donnelly', 'Angel Miller', 'Marcella Parks', 'Sara  Watkins', 'Shannon Stout', 'Lisa Duran', 'Erica Basilone', 'Carol Kish', 'Jennifer Talaski', 'Nicole Knisely',
-    'Caren Glowa', 'Johana Miller', 'Audrey Rogers', 'Cheri Shively', 'Amy Stayduhar', 'Dominique Huffman', 'Meaghan Wright', 'Kathryn Montano', 'Lynae Shepp', 'Anna Bell', 'Jessica Connole']]
-
-
-
-
-
-lead_name = "Jeremy Shock"
-"""start_date ='2018-12-17'
-end_date = '2018-12-21'
-week_df = get_inputs(start_date, end_date)
-week_df = week_df.sort_index(axis=1)
-writer = pd.ExcelWriter(start_date+'_input.xlsx')
-week_df.to_excel(writer, index = True)
-writer.save()"""
-
-"""for team in team_org:
-    lead_name = team[0]
-    print lead_name
-    
-    #Get team_df subset, clean it up, and save it as an excel file.
-    team_sliced = team[1:]
-    team_df = week_df[team_sliced]
-    team_df = team_df.sort_index(axis=1)
-    team_df.rename(columns={'*SSMax':'SSMax'}, inplace=True)
-    writer = pd.ExcelWriter(lead_name+'_input.xlsx')
-    team_df.to_excel(writer, index = True)
-    writer.save()
-
-    #Organize Excel File
-    wb = load_workbook(filename=lead_name+'_input.xlsx')
-    wb_sheet = wb['Sheet1']
-    wb_sheet.title = 'Raw Changes'
-
-
-    split_sheet_by_days(wb, skip_days, end_day_indicator)
-    call_create_tables(wb)
-    checks = {'Night Check': False, 'Day Check': False}
-    checks = define_blocks(wb, checks, scores)
-    df = checks[-1]
-    checks = checks[0]
-    data_library = find_non_empty_tables(wb, df)
-    create_summary_page(wb, data_library, checks)
-    wb.save(lead_name+'_leadbook.xlsx')
-    if lead_name =='All':
-        create_dataframe(data_library,wb, save_date)
-        save_leadbook(wb, save_date,debug, lead_name)
-    else:
-        create_books(wb,lead_name, save_date,debug)
-        save_leadbook(wb, save_date,debug, lead_name)"""
-
-
-
-
+debug = True
 
 """Calling Functions"""
-
-"""INPUTS HERE"""
-"""Jeremy Shock, Rachel Adams,Melissa Cox, Jill Szafranski,Kristin Donnelly,Caren Glowa, All"""
-#Uses Periscope Source and Tabby source to format and make the raw changes sheet in lead book.
-lead_name = "All"
-periscope = 'e-data_source/e-data_Fall/1217_all.csv'
-tabby = "e-data_source/e-data_Fall/1217_tabby.csv"
-week_df = create_input(periscope,tabby,lead_name)
-
 wb = load_workbook(filename='Input_EReport.xlsx')
 wb_sheet = wb['Sheet1']
 wb_sheet.title = 'Raw Changes'
