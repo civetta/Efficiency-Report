f2f3ec15dfa8c551b6540bab115cf1ad4e9811dd
Kelly Richardson <civettaxx@gmail.com>
2019-06-19 13:39:33 -0400

    initial results of 2to3

--- daily_ws/new_ereport.py
+++ daily_ws/new_ereport.py
@@ -1,10 +1,10 @@
 import time
 print('Enter name of team:')
-x = input()
-print "Collecting Data"
+x = eval(input())
+print("Collecting Data")
 time.sleep(3)
-print "Creating Leadbook"
+print("Creating Leadbook")
 time.sleep(3)
-print "Creating Teacher Books"
+print("Creating Teacher Books")
 time.sleep(3)
-print "Efficiency Report Complete"
+print("Efficiency Report Complete")
