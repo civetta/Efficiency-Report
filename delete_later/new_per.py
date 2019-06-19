b5f3b8de79b91d757fc7db417560d4b7930e0e37
Kelly Richardson <civettaxx@gmail.com>
2019-06-19 12:29:59 -0400

    Cleaning up

--- delete_later/new_per.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-
-lead_name = "All"
-"""Jeremy Shock, Rachel Adams,Jill Szafranski,Kristin Donnelly, Melissa Cox,Caren Glowa"""
-periscope = 'new_924.csv'
-tabby = 'tabby_924.csv'
